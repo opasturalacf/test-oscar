944f9deb15733be331e2af6a59f9293489b69414
opasturalacf <opastural@actioncontrelafaim.org>
2026-01-22 10:14:06 +0100

    Committing 4 items from workspace 19c4e82b-818b-4a84-b808-970799024b13

---
+++ Counter2.Notebook/notebook-content.py
@@ -0,0 +1,21 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {}
+# META }
+
+# CELL ********************
+
+mssparkutils.notebook.exit(int(2000))
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
